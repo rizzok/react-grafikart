e5cc507491cd9141f45182edd31ca3881aaf87e0
rizzok <38459379+rizzok@users.noreply.github.com>
2021-09-01 22:32:39 +0200

    C15

--- src/index.jsx
+++ src/index.jsx
@@ -1,73 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 
-function useIncrement (initialValue = 0, step = 1) {
-  const [count, setCount] = useState(initialValue)
-
-  const increment = function () {
-    setCount(c => c + 1)
-  }
-
-  return [
-    count, 
-    increment
-  ]
-}
-
-function useAutoIncrement (initialValue = 0, step = 1) {
-  const [value, setValue] = useState(initialValue)
-
-  return [value, increment]
-}
-
-function useToggle(initialValue = true) {
-  const [compteurVisible, setCompteurVisible] = useState(initialValue)
-  const toggleCompteur = function () {
-    setCompteurVisible(c => !c)
-  }
-  return [compteurVisible, toggleCompteur]
-}
-
 function Compteur () {
-  const [count, increment] = useAutoIncrement(10)
+  const [count, setCount] = useState(0)
 
-  return <button onClick={increment} className="compteur">Incrémenter {count}</button>
-}
-
-function App () {
-
-  // const [compteurVisible, toggleCompteur] = useToggle(true)
+  const handleClick = function (e) {
+    e.preventDefault()
+    setCount(10)
+  }
   
-  return <div>
-    {/* Afficher le compteur
-    <input type="checkbox" onChange={toggleCompteur} checked={compteurVisible} />
-    <br />
-    {compteurVisible && <Compteur />} */}
-    <TodoList />
-  </div>
+  return <button onClick={handleClick}>Nombre : {count}</button>
 }
 
-function TodoList () {
-  const [todos, setTodos] = useState([])
-
-  useEffect(function () {
-    (async function () {
-      const response = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
-      const responseData = await response.json()
-      if (response.ok) {
-        setTodos(responseData)
-      } else {
-        alert(JSON.stringify(responseData))
-      }
-    })()
-  }, [])
-  
-  return <div>
-    {todos.map(t => <li>{t.title}</li>)}
-  </div>
-}
-
-render (
-  <App />, 
-  document.getElementById('app')
+render(
+  <div>
+    <Compteur />
+  </div>,
+  document.getElementById('root')
 )
